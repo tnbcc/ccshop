ac791f1e172f8c135e653cbf80048de7fe5d28d8
vagrant <vagrant@homestead>
2018-07-18 07:21:01 +0000

    add 邮箱中间件

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
-Route::get('/','PagesController@root')->name('pages.root');
+
 
 Auth::routes();
+Route::get('/','PagesController@root')->name('pages.root');
+Route::group(['middleware'=>'auth'],function () {
+    Route::get('/email_verify_notice','PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::group(['middleware'=>'email_verified'],function () {
+              Route::get('/test',function () {
+                  return '邮箱已经认证';
+              });
 
-Route::get('/home', 'HomeController@index')->name('home');
+    });
+});
+
