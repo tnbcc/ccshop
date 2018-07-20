236d8fd758c4471bbffb24109adfb40b61d1f82d
tnbcc <tniub.cc@gmail.com>
2018-07-20 03:25:50 +0000

    新增商品编辑管理

--- database/migrations/2018_07_19_150718_create_product_skus_table.php
+++ database/migrations/2018_07_19_150718_create_product_skus_table.php
@@ -18,6 +18,7 @@ class CreateProductSkusTable extends Migration
             $table->string('title');
             $table->string('description');
             $table->decimal('price',10,2);
+            $table->unsignedInteger('stock');
             $table->unsignedInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
