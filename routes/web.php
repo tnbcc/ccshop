1ac2e29befc89b6de3ac8238044715574983bbd2
zhucong <tniub.cc@gmail.com>
2018-07-31 10:26:54 +0000

    管理后台-同意退款 -微信支付退款

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,8 @@ Route::group(['middleware'=>'auth'],function () {
 //服务器端回调
 Route::post('payment/alipay/notify','PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
+//微信退款回调
+Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
